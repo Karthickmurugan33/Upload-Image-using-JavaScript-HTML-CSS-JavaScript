fd71beff3293bdc3ec38b1d619f288b38723a26d
karthickmurugan33 <karrthick33@gmail.com>
2023-03-29 10:10:55 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,6 @@
+let image = document.querySelector("img");
+let input = document.querySelector("input");
+
+input.addEventListener("change", () => {
+    image.src = URL.createObjectURL(input.files[0]);
+})
